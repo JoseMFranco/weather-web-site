175b669ecb1eeb2b7f20492b7987d065302a8075
Jose Franco <jfranco@britghtcove.com>
2021-06-10 12:17:09 -0500

    setting up for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@ const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg1')
 const msgTwo = document.querySelector('#msg2')
 
-
-
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
@@ -13,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
   msgOne.textContent = 'Loading...'
   msgTwo.textContent = ''
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         msgOne.textContent = ''
